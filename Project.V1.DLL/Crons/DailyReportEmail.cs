90a27571e6fd6911c3e17541fbc9d120f61ba7ed
Adekunle Adeyemi <kunle03@hotmail.com>
2022-02-01 12:49:40 +0100

    Changed daily report recipient for production to real addresses

--- Project.V1.DLL/Crons/DailyReportEmail.cs
+++ Project.V1.DLL/Crons/DailyReportEmail.cs
@@ -19,7 +19,9 @@ namespace Project.V1.DLL.Crons
     {
         private readonly IRequest _request;
         private readonly List<string> tableColumnNames = new() { "S/N", "TECH", "SiteID", "Region", "Vendor", "Submission Date", "Acceptance Date", "Scope", "State" };
+        private readonly string RecipientsCSV = "chinedu.obi@ericsson.com,mtnnocsl@huawei.com,akinola@huawei.com,Joseph.Yakubu@mtn.com,he.jin2@zte.com.cn,Abraham.Uanzekin@mtn.com,Akeem.Alabi@mtn.com,Harold.Obodozie@mtn.com,Olayinka.Esan@mtn.com,Olabode.Aluko@mtn.com,Peter.Okewumi@mtn.com,Oladipo.Bajo@mtn.com,#3G-NOCBackOffice.NG@mtn.com,muhammsal@gmail.com,Nnamdi.Ugochukwu@mtn.com,Babatunde.Ayeni@mtn.com,David.Melaiye@mtn.com,Henry.Obukoadata@mtn.com,lei.yifang@zte.com.cn,Abdul.Ajikobi@mtn.com,#TransmissionBearerServices.NG@mtn.com,#TransmissionAccessPlanningNorth.NG@mtn.com,#TransmissionAccessPlanningWest.NG@mtn.com,#NIDPSOReports.NG@mtn.com,gao.shuang@zte.com.cn,nity.dangwal@zte.com.cn,zengruile@huawei.com,iakhidenor@gmail.com,zhang.yabo111@zte.com.cn,#MTNNigeriaTSSNWG.NG@mtn.com,leey.liyi@huawei.com,#TransmissionAccessPlanningEast.NG@mtn.com,cui.haibo5@zte.com.cn,Chinedu.Ezeigweneme@mtn.com,#RFOptimization.NG@mtn.com,Esther.Igbinakenzua@mtn.com,frederick.kpam@ericsson.com,chido@molcomconcepts.com,stephen.caoguodong@huawei.com,Abayomi.Onafuye@mtn.com,Orieke.Nwosu@mtn.com,femaj2001@yahoo.com,nnamdi.osuji@huawei.com,stephen.seyi.ademoloye@ericsson.com,tosin.adedapo@ericsson.com,adebayo.sulaiman.oshijirin@ericsson.com,Joy.Okpo@mtn.com,Idongesit.Udom@mtn.com,samuel.ola1@huawei.com,irorere.lawrence.osakhienuwa@huawei.com,ejiofor.asogwa@ericsson.com,satish.satish@zte.com.cn,tang.mingxin1@zte.com.cn,Olufunso.Oluwapojuwomi@mtn.com,amah.Jackson@mtn.com,#NIDRAI.NG@mtn.com,tola.daramola@ericsson.com,ragavendra.kumar@ericsson.com,oluwadare.awe@ericsson.com,abimbola.nwankwonta@ericsson.com,eddie.zhangfan@huawei.com,Olayemi.Awofisoye@huawei.com,olawale.aminu@huawei.com,femi.ajayi@zte.com.cn,#NWGUAT_FAT.NG@mtn.com,oghenekevwe.kofi@ericsson.com,kehinde.akingbagbohun@huawei.com,patrick.okaka@huawei.com,ekene.anthony.ibedu@huawei.com,#RFPlanningEngr.NG@mtn.com,uzoma.joenkamuke@huawei.com,Oluwaseun.Onabajo@huawei.com,duqisheng@huawei.com,imoh.umobong@ericsson.com,asuku.aliu.mohammed@ericsson.com,nokia-opt@list.nokia.com,Titilayo.Oguntokun@mtn.com,maxz.chooi@huawei.com,osukoya.ayodele@huawei.com,bola.badie.zaki@huawei.com,alabi.shukrat.mopelola@huawei.com,Young.Omereonye@mtn.com,yakubu.oke.ext@nokia.com,muhammad.t.khan.ext@nokia.com,MohammadReza.Rajabi@mtn.com,oyebode.olumide.temitayo@huawei.com,Rasheed.Bello@mtn.com,Kayode.Olufuwa@mtn.com,Ayodeji.Oni@mtn.com,Peter.Erin@mtn.com,ogunbiyi.timilehin.oladapo@huawei.com,Albert.Chukwuma@mtn.com,augustine.solomon@huawei.com,xue.ningyi@zte.com.cn,hu.shaodong@zte.com.cn,Iyilary@zte.com.cn,wang.huiwen30@zte.com.cn,peng.weidong@zte.com.cn,liu.gang5@zte.com.cn,mohd.zuheb.shakeel@ericsson.com,adeboye.dayo@huawei.com,zhanghaitao11@huawei.com,wangguangxi@huawei.com,pengzhenxing@huawei.com,hazem.amaher@huawei.com,Adeniran.Adepoju@mtn.com,Muhammad.Ashraf@mtn.com,Adeel.Ahmed1@mtn.com,#TxAccessPlanningHQ.NG@mtn.com,idrisolanigan@gmail.com,jesse.obuotor@nokia.com,adebanji.adeyemi@nokia.com,yuguda.hamisu.ext@nokia.com,fehintola.olayemi.ext@nokia.com,waqar.mehmood@nokia.com,chijioke.okoli@nokia.com,huzhili@huawei.com,Tochukwu.Alaekee@mtn.com,#MTNNigeriaTSSNWG.NG@mtn.com,#networkaccessplanning&optimizationhq.ng@mtn.com";
         private Dictionary<string, int> TotalRow = new();
+        private readonly string ENV = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
         private readonly Dictionary<string, string> TableProperties = new()
         {
@@ -120,12 +122,17 @@ namespace Project.V1.DLL.Crons
             mvm.Greetings += $"<p>In {yesterDay:MMMM yyyy}, <b>total of {TotalRow["2G"]} 2G, {TotalRow["3G"]} 3G, {TotalRow["Multi Sector"]} Multi-Sector, {TotalRow["U900"]} U900,  {TotalRow["700M"]} LTE7, {TotalRow["800M"]} LTE8, {TotalRow["1800M"]} LTE18, {TotalRow["2600M"]} LTE26</b> ARE RF ATP-ed:</p>";
             mvm.Greetings += $"<p> &nbsp; </p>";
 
-            mvm.To = new List<SenderBody>
+            var devRecipient = new List<SenderBody>
             {
                 new SenderBody { Name = "", Address = "Adekunle.Adeyemi@mtn.com" },
             };
+
+            var recipientList = (ENV == "Development1") ? devRecipient : HelperFunctions.ConvertMailStringToList(RecipientsCSV);
+
+            mvm.To = recipientList;
             mvm.CC = new List<SenderBody>
             {
+                new SenderBody { Name = "", Address = "Adekunle.Adeyemi@mtn.com" },
                 new SenderBody { Name = "", Address = "Anthony.Nwosu@mtn.com" },
                 new SenderBody { Name = "", Address = "Kehinde.Ayoola-Oni@mtn.com" },
             };
